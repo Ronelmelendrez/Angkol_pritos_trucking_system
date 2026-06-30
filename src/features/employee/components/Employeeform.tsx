bf01b4981c65fb33b8bde93fe5d5333914689c60
Ronel <ronelmelendrez01@gmail.com>
2026-07-01 00:02:53 +0800

    fixed: the directory importing ui switch

--- src/features/employee/components/Employeeform.tsx
+++ src/features/employee/components/Employeeform.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Switch } from "@/components/ui/switch"
+import { Switch } from "@/components/ui/Switch"
 import {
   Form,
   FormControl,
