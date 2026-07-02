3d9e3e619c4edebf21e5df7ea14ce45fc1706ee3
Ronel <ronelmelendrez01@gmail.com>
2026-07-03 01:42:59 +0800

    feat: add exporter index on employee feature

---
+++ src/features/employee/index.ts
@@ -0,0 +1,10 @@
+export { EmployeeForm } from "./components/EmployeeForm";
+export { EmployeeList } from "./components/EmployeeList";
+export { EmployeeCard } from "./components/EmployeeCard";
+export {
+  useEmployees,
+  useAddEmployee,
+  useUpdateEmployee,
+  useDeleteEmployee,
+} from "./hooks/useEmployees";
+export type { Employee, NewEmployee, UpdateEmployee } from "./types";
