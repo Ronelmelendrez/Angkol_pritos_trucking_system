f7d6c784d0bb3153f38d9a04bac4dbb31a04c579
Ronel <ronelmelendrez01@gmail.com>
2026-07-03 01:58:26 +0800

    feat: add a exporter index on attendance feature

---
+++ src/features/attendance/index.ts
@@ -0,0 +1,5 @@
+export { ClockInOutButton } from "./components/ClockInOutButton";
+export { AttendanceLog } from "./components/AttendanceLog";
+export { AttendanceCalendar } from "./components/AttendanceCalendar";
+export { useAttendance, useClockIn, useClockOut } from "./hooks/useAttendance";
+export type { AttendanceRecord, NewAttendanceRecord } from "./types";
