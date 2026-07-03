85f0eca97ae8a0af2b2d81eb9d0a1bba95c0f8e1
Ronel <ronelmelendrez01@gmail.com>
2026-07-03 21:32:11 +0800

    feat: add exporter index on advance feature

---
+++ src/features/advance/index.ts
@@ -0,0 +1,9 @@
+export { AdvanceForm } from "./components/AdvanceForm";
+export { AdvancesList } from "./components/AdvancesList";
+export {
+  useAdvances,
+  useAddAdvance,
+  useMarkAdvanceDeducted,
+  useDeleteAdvance,
+} from "./hooks/useAdvances";
+export type { CashAdvance, NewCashAdvance } from "./types";
