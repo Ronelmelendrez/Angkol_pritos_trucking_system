cb58f332226d0d1cc70820831c341306e1b1964d
Ronel <ronelmelendrez01@gmail.com>
2026-07-03 21:38:34 +0800

    feat: add exporter index on loans feature

---
+++ src/features/loans/index.ts
@@ -0,0 +1,5 @@
+export { LoanForm } from "./components/LoanForm";
+export { LoansList } from "./components/LoansList";
+export { RepaymentForm } from "./components/RepaymentForm";
+export { useLoans, useRepayments, useAddLoan, useRepayLoan } from "./hooks/useLoans";
+export type { Loan, NewLoan, Repayment, NewRepayment } from "./types";
