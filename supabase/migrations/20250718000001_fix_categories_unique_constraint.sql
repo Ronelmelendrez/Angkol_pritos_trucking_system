ee9c0e729f1f2c786804bc126a0742353287535f
Ronel <ronelmelendrez01@gmail.com>
2026-07-25 23:15:59 +0800

    refactor: delete migration do not needed

--- supabase/migrations/20250718000001_fix_categories_unique_constraint.sql
+++
@@ -1,4 +0,0 @@
--- Fix categories unique constraint: allow same name for different types
-
-ALTER TABLE categories DROP CONSTRAINT categories_name_key;
-ALTER TABLE categories ADD CONSTRAINT categories_name_type_unique UNIQUE (name, type);
