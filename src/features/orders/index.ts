f19a49c89fbb0505ecf966110a4ee54cb9920cb5
Ronel <ronelmelendrez01@gmail.com>
2026-08-21 00:09:37 +0800

    feat: add a export barrel for employee features

---
+++ src/features/orders/index.ts
@@ -0,0 +1,10 @@
+export { OrderForm } from "./components/OrderForm";
+export { OrdersList } from "./components/OrdersList";
+export {
+  useOrders,
+  useAddOrder,
+  useUpdateOrder,
+  useDeleteOrder,
+  useClaimOrder,
+} from "./hooks/useOrders";
+export type { Order, NewOrder, UpdateOrder, OrderItem } from "./types";
