28d6900368d2d052413e993c59f306d5c77d7dad
Ronel <ronelmelendrez01@gmail.com>
2026-08-22 00:03:13 +0800

    refactor: export all new dialog for order confirmation ect

--- src/features/orders/index.ts
+++ src/features/orders/index.ts
@@ -4,11 +4,21 @@ export { OrderStats } from "./components/OrderStats";
 export { OrderGridCard } from "./components/OrderGridCard";
 export { OrderFiltersBar } from "./components/OrderFilters";
 export { OrderDetailDialog } from "./components/OrderDetailDialog";
+export { OrderConfirmationDialog } from "./components/OrderConfirmationDialog";
+export { OrderCancellationDialog } from "./components/OrderCancellationDialog";
+export { OrderCompletionDialog } from "./components/OrderCompletionDialog";
 export {
   useOrders,
   useAddOrder,
   useUpdateOrder,
   useDeleteOrder,
-  useClaimOrder,
+  useCompleteOrder,
+  useCancelOrder,
 } from "./hooks/useOrders";
-export type { Order, NewOrder, UpdateOrder, OrderItem } from "./types";
+export {
+  useOrderPayments,
+  useAllOrderPayments,
+  useAddOrderPayment,
+  useDeleteOrderPayment,
+} from "./hooks/useOrderPayments";
+export type { Order, NewOrder, UpdateOrder, OrderItem, OrderPayment, NewOrderPayment, PaymentType } from "./types";
