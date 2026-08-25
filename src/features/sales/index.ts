1f1458e3948d00ec8ee16d4b19995abee9f0f9be
Ronel <ronelmelendrez01@gmail.com>
2026-08-25 23:33:59 +0800

    refactor: add the new component int he sales export barrel

--- src/features/sales/index.ts
+++ src/features/sales/index.ts
@@ -1,9 +1,14 @@
 export { SaleForm } from "./components/SaleForm";
 export { SalesList } from "./components/SalesList";
+export { BranchSalesDashboard } from "./components/BranchSalesDashboard";
 export {
   useSales,
   useAddSale,
   useUpdateSale,
   useDeleteSale,
 } from "./hooks/useSales";
+export {
+  useBranchSalesSummary,
+  useBranchSalesComparison,
+} from "./hooks/useBranchSales";
 export type { Sale, NewSale, UpdateSale } from "./types";
